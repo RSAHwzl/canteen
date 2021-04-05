9fb1cb19dc8b2a862df1a66dfecb9e60ea1f5684
wzl <wzl@wzl.com>
2021-04-05 11:05:40 +0800

    ajax设置

---
+++ src/main/java/com/lin/canteen/config/WebMvcConfig.java
@@ -0,0 +1,18 @@
+package com.lin.canteen.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override//跨域支持
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+        corsRegistry.addMapping("/**")
+                .allowedMethods("*")
+                .allowedOrigins("*")
+                .allowCredentials(true)
+                .allowedHeaders("*");
+    }
+}
